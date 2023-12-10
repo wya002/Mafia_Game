ec5ba3778280d41cccf4f3c6e46709aac57a01d3
wya002 <dndyddks001@naver.com>
2023-12-11 01:01:53 +0900

    feat : add GameClient

--- src/game/server/GameClient.java
+++ src/game/server/GameClient.java
@@ -4,52 +4,43 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 
-public class GameServer{
+public class GameClient {
+
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
-		
 		BufferedReader in = null;
 		PrintWriter out = null;
 		
-		ServerSocket serverSocket = null;
 		Socket socket = null;
 		Scanner scanner = new Scanner(System.in);
 		
 		try {
-			serverSocket = new ServerSocket(8080);
+			socket = new Socket("127.0.0.1", 8080);
 			
-			System.out.println("[Server실행] Client 연결대기중...");
-			socket = serverSocket.accept();
-			
-			System.out.println("Client 연결됨.");
 			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			out = new PrintWriter(socket.getOutputStream());
 			
-			while(true){
-				String inputMessage = in.readLine();
-				if("quit".equalsIgnoreCase(inputMessage))break;
-				
-				System.out.println("From Client : " + inputMessage);
-				System.out.print("전송하기 >>> ");
-				
+			while(true) {
+				System.out.print("전송하기>>> ");
 				String outputMessage = scanner.nextLine();
 				out.println(outputMessage);
 				out.flush();
-				if("quit".equalsIgnoreCase(outputMessage))break;
+				if ("quit".equalsIgnoreCase(outputMessage)) break;
+								
+				String inputMessage = in.readLine();
+				System.out.println("From Server: " + inputMessage);
+				if ("quit".equalsIgnoreCase(inputMessage)) break;
 			}
 		} catch (IOException e) {
-			e.printStackTrace();
+			System.out.println(e.getMessage());
 		} finally {
 			try {
 				scanner.close();
-				socket.close();
-				serverSocket.close();
-				System.out.println("연결종료");
-			}catch(IOException e) {
+				if (socket != null) socket.close();
+				System.out.println("서버연결종료");
+			} catch (IOException e) {
 				System.out.println("소켓통신에러");
 			}
 		}
